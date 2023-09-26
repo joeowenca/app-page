995bcbf4baa04bb278bb90aa7890080bc0322134
Joe Owen <joe.a.owen@gmail.com>
2023-09-26 15:25:28 -0700

    Highlight app when selected in Edit Modal

--- src/app/edit/default-apps-manifest.ts
+++ src/app/edit/default-apps-manifest.ts
@@ -32,6 +32,7 @@ export type DefaultAppTypes = {
   name: string;
   url: string;
   icon: StaticImageData;
+  active: boolean;
 }
 
 export const defaultApps: DefaultAppTypes[] = [
@@ -39,135 +40,162 @@ export const defaultApps: DefaultAppTypes[] = [
     name: "Amazon",
     url: "https://www.amazon.ca/",
     icon: amazon,
+    active: false,
   },
   {
     name: "Asana",
     url: "https://app.asana.com/",
     icon: asana,
+    active: false,
   },
   {
     name: "BambooHR",
     url: "https://www.bamboohr.com/",
     icon: bamboohr,
+    active: false,
   },
   {
     name: "Best Buy",
     url: "https://www.bestbuy.ca/",
     icon: bestbuy,
+    active: false,
   },
   {
     name: "BrowserStack",
     url: "https://www.browserstack.com/",
     icon: browserstack,
+    active: false,
   },
   {
     name: "Google Calendar",
     url: "https://calendar.google.com/",
     icon: googlecalendar,
+    active: false,
   },
   {
     name: "Confluence",
     url: "https://www.atlassian.com/software/confluence",
     icon: confluence,
+    active: false,
   },
   {
     name: "Craigslist",
     url: "https://geo.craigslist.org/",
     icon: craigslist,
+    active: false,
   },
   {
     name: "Google Docs",
     url: "https://docs.google.com/",
     icon: googledocs,
+    active: false,
   },
   {
     name: "Google Drive",
     url: "https://drive.google.com/",
     icon: googledrive,
+    active: false,
   },
   {
     name: "eBay",
     url: "https://www.ebay.ca/",
     icon: ebay,
+    active: false,
   },
   {
     name: "Facebook",
     url: "https://www.facebook.com/",
     icon: facebook,
+    active: false,
   },
   {
     name: "Gmail",
     url: "https://mail.google.com/",
     icon: gmail,
+    active: false,
   },
   {
     name: "Google",
     url: "https://www.google.ca/",
     icon: google,
+    active: false,
   },
   {
     name: "Jira",
     url: "https://www.atlassian.com/software/jira",
     icon: jira,
+    active: false,
   },
   {
     name: "Memory Express",
     url: "https://www.memoryexpress.com/",
     icon: memoryexpress,
+    active: false,
   },
   {
     name: "Messenger",
     url: "https://www.facebook.com/messages",
     icon: messenger,
+    active: false,
   },
   {
     name: "Netflix",
     url: "https://www.netflix.com/browse",
     icon: netflix,
+    active: false,
   },
   {
     name: "Google Photos",
     url: "https://photos.google.com/",
     icon: googlephotos,
+    active: false,
   },
   {
     name: "Slack",
     url: "https://app.slack.com/client/",
     icon: slack,
+    active: false,
   },
   {
     name: "SoundCloud",
     url: "https://soundcloud.com/feed",
     icon: soundcloud,
+    active: false,
   },
   {
     name: "Google Sheets",
     url: "https://docs.google.com/spreadsheets/",
     icon: googlesheets,
+    active: false,
   },
   {
     name: "Used",
     url: "https://www.used.ca/",
     icon: used,
+    active: false,
   },
   {
     name: "World Time Buddy",
     url: "https://www.worldtimebuddy.com/",
     icon: worldtimebuddy,
+    active: false,
   },
   {
     name: "YouTube",
     url: "https://www.youtube.com/",
     icon: youtube,
+    active: false,
   },
   {
     name: "Zendesk",
     url: "https://www.zendesk.com/",
     icon: zendesk,
+    active: false,
   },
   {
     name: "Zoom",
     url: "https://zoom.us/",
     icon: zoom,
+    active: false,
   },
 ];
--- src/app/edit/default-apps.tsx
+++ src/app/edit/default-apps.tsx
@@ -1,14 +1,39 @@
+"use client";
+
+import { useState } from "react";
 import { defaultApps, DefaultAppTypes } from "./default-apps-manifest";
 import { AppItem } from "../apppage";
 
 export default function DefaultApps() {
+  const [apps, setApps] = useState<DefaultAppTypes[]>(defaultApps);
+
+  function setActive(name: string) {
+    const updatedApps = structuredClone(apps);
+
+    updatedApps.map((app: DefaultAppTypes) => {
+      if (app.name === name) {
+        app.active = true;
+      } else {
+        app.active = false;
+      }
+    });
+
+    setApps(updatedApps);
+  }
+
   return (
     <div className="p-5 flex flex-col">
       <h1 className="pb-5 text-xl">Default Apps</h1>
       <div className="rounded-2xl outline outline-4 outline-zinc-800 overflow-hidden">
         <div className="max-h-96 grid grid-cols-4 grid-flow-row gap-8 p-8 overflow-y-auto">
-          {defaultApps.map((app: DefaultAppTypes) => (
-            <AppItem name={app.name} icon={app.icon} key={app.name} />
+          {apps.map((app: DefaultAppTypes) => (
+            <AppItem
+              name={app.name}
+              icon={app.icon}
+              url={setActive}
+              key={app.name}
+              active={app.active}
+            />
           ))}
         </div>
       </div>
