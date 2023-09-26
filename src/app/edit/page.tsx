a110b9e08eda23fa2ac237c9d1e81ca080b28a6e
Joe Owen <joe.a.owen@gmail.com>
2023-09-25 21:41:04 -0700

    Style edit modal buttons

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -28,11 +28,23 @@ function EditModal({ show, setShow }: EditModalProps) {
           : "opacity-0 pointer-events-none"
       } z-10 fixed flex justify-center items-center w-full h-full transition-opacity`}
     >
-      <div className="flex flex-col p-12 bg-blue-950 max-w-2xl rounded-2xl">
-        <div className="">Edit modal</div>
-        <div className="flex">
-          <Button onClick={() => setShow(false)}>Save</Button>
-          <Button onClick={() => setShow(false)}>Close</Button>
+      <div className="flex flex-col items-center p-5 bg-blue-950 max-w-2xl rounded-2xl">
+        <div className="px-24 py-12">Edit modal</div>
+        <div className="flex w-full">
+          <div className="flex">
+            <Button
+              className="hover:bg-blue-800"
+              onClick={() => setShow(false)}
+            >
+              Save
+            </Button>
+            <Button
+              className="text-slate-500 hover:text-white hover:bg-blue-800"
+              onClick={() => setShow(false)}
+            >
+              Cancel
+            </Button>
+          </div>
         </div>
       </div>
     </div>
@@ -40,13 +52,17 @@ function EditModal({ show, setShow }: EditModalProps) {
 }
 
 type ButtonProps = {
-  children: string;
+  className: string;
   onClick: MouseEventHandler<HTMLDivElement>;
+  children: string;
 };
 
-function Button({ children, onClick }: ButtonProps) {
+function Button({ className, onClick, children }: ButtonProps) {
   return (
-    <div onClick={onClick} className="p-2 hover:cursor-pointer">
+    <div
+      onClick={onClick}
+      className={`p-2 mx-1 hover:cursor-pointer transition-colors rounded-lg ${className}`}
+    >
       {children}
     </div>
   );
