fa12f40a4ec4719415e6d1365e9894966987d500
Joe Owen <joe.a.owen@gmail.com>
2023-09-26 15:37:09 -0700

    Improve highlighted app logic

--- src/app/edit/default-apps.tsx
+++ src/app/edit/default-apps.tsx
@@ -5,20 +5,12 @@ import { defaultApps, DefaultAppTypes } from "./default-apps-manifest";
 import { AppItem } from "../apppage";
 
 export default function DefaultApps() {
-  const [apps, setApps] = useState<DefaultAppTypes[]>(defaultApps);
+  const [activeApp, setActiveApp] = useState<DefaultAppTypes | null>(null);
 
   function setActive(name: string) {
-    const updatedApps = structuredClone(apps);
-
-    updatedApps.map((app: DefaultAppTypes) => {
-      if (app.name === name) {
-        app.active = true;
-      } else {
-        app.active = false;
-      }
-    });
-
-    setApps(updatedApps);
+    setActiveApp(
+      defaultApps.find((app: DefaultAppTypes) => app.name === name) ?? null
+    );
   }
 
   return (
@@ -26,13 +18,13 @@ export default function DefaultApps() {
       <h1 className="pb-5 text-xl">Default Apps</h1>
       <div className="rounded-2xl outline outline-4 outline-zinc-800 overflow-hidden">
         <div className="max-h-96 grid grid-cols-4 grid-flow-row gap-8 p-8 overflow-y-auto">
-          {apps.map((app: DefaultAppTypes) => (
+          {defaultApps.map((app: DefaultAppTypes) => (
             <AppItem
               name={app.name}
               icon={app.icon}
               url={setActive}
               key={app.name}
-              active={app.active}
+              active={app === activeApp}
             />
           ))}
         </div>
