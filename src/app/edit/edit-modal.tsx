480f5ad0636080d62cc7beb78f0f96fee822f57d
Joe Owen <joe.a.owen@gmail.com>
2023-09-26 09:30:23 -0700

    Update edit modal transition animation

--- src/app/edit/edit-modal.tsx
+++ src/app/edit/edit-modal.tsx
@@ -10,9 +10,9 @@ export default function EditModal({ show, setShow }: EditModalProps) {
     <div
       className={`${
         show
-          ? "opacity-100 pointer-events-auto"
-          : "opacity-0 pointer-events-none"
-      } z-10 fixed flex justify-center items-center w-full h-full transition-opacity`}
+          ? "opacity-100 translate-y-0 pointer-events-auto"
+          : "opacity-0 translate-y-4 pointer-events-none"
+      } z-10 fixed flex justify-center items-center w-full h-full transition-all duration-300`}
     >
       <div className="flex flex-col items-center p-5 bg-blue-950 max-w-2xl rounded-2xl">
         <div className="px-24 py-12">Edit modal</div>
