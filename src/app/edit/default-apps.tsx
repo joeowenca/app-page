54533b26c55b8e3cdcedc3b0b1225f6079eaa3c2
Joe Owen <joe.a.owen@gmail.com>
2023-09-26 15:59:51 -0700

    Show app details with input forms

--- src/app/edit/default-apps.tsx
+++ src/app/edit/default-apps.tsx
@@ -14,8 +14,8 @@ export default function DefaultApps() {
   }
 
   return (
-    <div className="p-5 flex flex-col">
-      <h1 className="pb-5 text-xl">Default Apps</h1>
+    <div className="p-5 flex flex-col transition-all">
+      <h1 className="pb-5 text-xl">Add App</h1>
       <div className="rounded-2xl outline outline-4 outline-zinc-800 overflow-hidden">
         <div className="max-h-96 grid grid-cols-4 grid-flow-row gap-8 p-8 overflow-y-auto">
           {defaultApps.map((app: DefaultAppTypes) => (
@@ -28,6 +28,30 @@ export default function DefaultApps() {
             />
           ))}
         </div>
+      </div>
+      <div>
+        {activeApp ? (
+          <>
+            <h1 className="py-5 text-xl">App Details</h1>
+            <form>
+              <label className="pr-3">Name</label>
+              <input
+                className="transition-colors outline-0 hover:cursor-text bg-zinc-800 hover:bg-zinc-700 focus:bg-zinc-700 p-2 mb-5 rounded-xl"
+                type="text"
+                id="name"
+                value={activeApp.name}
+              ></input>
+              <br />
+              <label className="pr-3">URL</label>
+              <input
+                className="transition-colors outline-0 hover:cursor-text bg-zinc-800 hover:bg-zinc-700 focus:bg-zinc-700 p-2 mb-5 w-96 rounded-xl"
+                type="text"
+                id="name"
+                value={activeApp.url}
+              ></input>
+            </form>
+          </>
+        ) : null}
       </div>
     </div>
   );
