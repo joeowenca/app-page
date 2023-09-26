54a95c40b6f56718f2e45eed802b592cf3d4b002
Joe Owen <joe.a.owen@gmail.com>
2023-09-25 20:13:48 -0700

    Create AppItem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image, { StaticImageData } from "next/image";
+
 type AppPageProps = {
   edit: boolean;
 };
@@ -5,13 +7,30 @@ type AppPageProps = {
 export default function AppPage({ edit }: AppPageProps) {
   return (
     <div className="flex justify-center w-full">
-      <div className="p-5">{edit ? <AppItem /> : ""}</div>
+      <div className="p-5">
+        {edit ? <AppItem name="Edit" edit={true} /> : ""}
+      </div>
     </div>
   );
 }
 
-function AppItem() {
+type AppItemProps = {
+  name: string;
+  image?: StaticImageData;
+  url?: string;
+  edit?: boolean;
+};
+
+function AppItem({ name, image, url, edit }: AppItemProps) {
   return (
-    <div className="icon-plus text-5xl p-8 cursor-pointer transition-all duration-75 hover:outline outline-4 outline-blue-500 hover:bg-white/[15%] rounded-2xl"></div>
+    <a href={url}>
+      <div
+        className={`${
+          edit ? "icon-plus" : null
+        } text-5xl p-8 cursor-pointer transition-all duration-75 hover:outline outline-4 outline-blue-500 hover:bg-white/[15%] rounded-2xl`}
+      >
+        {edit ? null : image ? <Image src={image.src} alt={name} /> : null}
+      </div>
+    </a>
   );
 }
