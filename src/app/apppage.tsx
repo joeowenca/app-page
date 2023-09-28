fd40dec011629903e429a1021a5d49c78137167c
Joe Owen <joe.a.owen@gmail.com>
2023-09-27 19:09:54 -0700

    Show added apps on App Page component

--- src/app/apppage.tsx
+++ src/app/apppage.tsx
@@ -10,7 +10,7 @@ type AppPageProps = {
 export default function AppPage({ apps, edit, setModal }: AppPageProps) {
   return (
     <div className="flex justify-center w-full">
-      <div className="p-5">
+      <div className="grid grid-cols-6 grid-flow-row gap-8 p-8 items-center">
         {apps
           ? apps?.map((app: AppTypes) => (
               <AppItem name={app.name} icon={app.icon} url={app.url}></AppItem>
@@ -19,7 +19,9 @@ export default function AppPage({ apps, edit, setModal }: AppPageProps) {
 
         {edit ? (
           <div onClick={() => (setModal ? setModal(true) : null)}>
-            <AppItem name="Edit" edit={true} />
+            <div className="relative aspect-square w-[120px] cursor-pointer transition-all duration-75 hover:outline outline-4 outline-blue-500 hover:bg-white/[15%] rounded-2xl">
+              <div className="icon-plus text-5xl"></div>
+            </div>
           </div>
         ) : null}
       </div>
@@ -31,22 +33,21 @@ type AppItemProps = {
   name: string;
   icon?: StaticImageData;
   url?: string | Function;
-  edit?: boolean;
   active?: boolean;
 };
 
-function AppItem({ name, icon, url, edit, active }: AppItemProps) {
+function AppItem({ name, icon, url, active }: AppItemProps) {
   return (
     <a
       href={typeof url === "string" ? url : "#"}
       onClick={() => (typeof url === "function" ? url(name) : null)}
     >
       <div
-        className={`${edit ? "icon-plus w-auto" : null} ${
+        className={`${
           active ? "outline bg-white/[15%]" : null
-        } text-5xl w-[120px] cursor-pointer transition-all duration-75 hover:outline outline-4 outline-blue-500 hover:bg-white/[15%] rounded-2xl`}
+        } w-[120px] cursor-pointer transition-all duration-75 hover:outline outline-4 outline-blue-500 hover:bg-white/[15%] rounded-2xl`}
       >
-        {edit ? null : icon ? <Image src={icon} alt={name} /> : null}
+        {icon ? <Image src={icon} alt={name} /> : null}
       </div>
     </a>
   );
