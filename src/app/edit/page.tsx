ae9c668b515c2203514ffc898fad23ae383278fc
Joe Owen <joe.a.owen@gmail.com>
2023-10-06 16:48:40 -0700

    Refactor deleteApp functions into separate file

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -6,6 +6,12 @@ import AppPage from '../apppage';
 import Modal from './modal';
 import UndoModal from './undo';
 import { AppTypes } from '../apps-manifest';
+import {
+	clearAppDetails,
+	addDeletedApp,
+	purgeApp,
+	purgeDeletedApp,
+} from './delete-app';
 
 export default function Edit() {
 	const [showModal, setShowModal] = useState(false);
@@ -27,40 +33,16 @@ export default function Edit() {
 		const appIndex = apps.findIndex((app: AppTypes) => app.id === id);
 		const deletedApp = apps.find((app: AppTypes) => app.id === id);
 
-		function clearAppDetails(index: number) {
-			const updatedApps = [...apps];
-			updatedApps[index].details = undefined;
-			setApps(updatedApps);
-		}
-
-		function purgeApp(index: number) {
-			apps.splice(index, 1);
-			setApps(apps);
-		}
-
-		function addDeletedApp(app: AppTypes) {
-			deletedApps.push(app);
-			setDeletedApps(deletedApps);
-		}
-
-		function purgeDeletedApp() {
-			const index = deletedApps.findIndex(
-				(app: AppTypes) => app.id === id,
-			);
-			deletedApps.splice(index, 1);
-			setDeletedApps(deletedApps);
-		}
-
 		function delayedDelete() {
-			purgeApp(appIndex);
-			purgeDeletedApp();
+			setApps(purgeApp(appIndex, apps));
+			setDeletedApps(purgeDeletedApp(id, deletedApps));
 		}
 
 		if (appIndex !== -1) {
-			clearAppDetails(appIndex);
+			setApps(clearAppDetails(appIndex, apps));
 
 			if (deletedApp) {
-				addDeletedApp(deletedApp);
+				setDeletedApps(addDeletedApp(deletedApp, deletedApps));
 			}
 
 			setTimeout(() => delayedDelete(), 10000);
