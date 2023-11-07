e37b7898e01073c165035a6177b000a2da4a8c83
Joe Owen <joe.owen@checkfront.com>
2023-11-06 19:02:30 -0800

    Add Menu component

---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,7 @@
+export default function Menu() {
+	return (
+		<div className="fixed bottom-0 w-full p-5">
+			<div className="icon-cog float-right text-3xl text-zinc-600 hover:text-white transition-colors duration-75"></div>
+		</div>
+	);
+}
