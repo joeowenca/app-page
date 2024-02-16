396a86b401cf33b75613925fffc746ea355490d3
Joe Owen <joe.owen@checkfront.com>
2024-02-15 20:53:16 -0800

    Pass categoryId prop to AppItem component

--- src/app/edit/components/AppLibrary.tsx
+++ src/app/edit/components/AppLibrary.tsx
@@ -14,12 +14,13 @@ export default function AppLibrary({ onClick, activeApp }: AppLibraryProps) {
 				className="max-h-[32rem] grid grid-cols-4 grid-flow-row gap-8 p-8 overflow-y-auto"
 			>
 				{apps.map((app: AppTypes) =>
-					app.details ? (
+					app.details && app.categoryId ? (
 						<AppItem
 							name={app.details.name}
 							icon={app.details.icon}
 							onClick={onClick}
 							id={app.id}
+							categoryId={app.categoryId}
 							key={app.id}
 							active={
 								app.details.icon === activeApp?.details.icon
